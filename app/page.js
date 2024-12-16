79f53c6589134dbe1846cc597f48819a23e9d35e
Rachel Gina <rachel.gina@foodpanda.com>
2024-12-16 23:24:54 +0800

    Add cuisine carousel and associated mock data

--- app/page.js
+++ app/page.js
@@ -1,15 +1,21 @@
+"use client";
+
 import Header from "../components/Header";
+import CuisineCarousel from "../components/tapau_page/CuisineCarousel";
 
 export default function HomePage() {
   return (
     <>
       <Header logoUrl="/assets/images/header-logo.png" />
-      <main>
-        <img
-          src="/assets/images/tapau-together-banner.png"
-          alt="TaPau Together Banner"
-          className={"w-full flex items-center justify-between p-10 radius-2xl"}
-        />
+      <main className="p-5 w-11/12 items-center mx-auto my-8">
+        <div className="">
+          <img
+            src="/assets/images/tapau-together-banner.png"
+            alt="TaPau Together Banner"
+            className="w-full rounded-2xl"
+          />
+        </div>
+        <CuisineCarousel />
       </main>
     </>
   );
