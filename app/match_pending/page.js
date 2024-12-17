eae10e5966806cc300ca46f55a4a756c2f1ac44f
Rachel Gina <rgabelarde@gmail.com>
2024-12-17 15:37:36 +0800

    Fix match pending page back button to show cancellation modal

--- app/match_pending/page.js
+++ app/match_pending/page.js
@@ -3,14 +3,27 @@
 import BackButton from "@/components/ui/BackButton";
 import Header from "../../components/Header";
 import CountdownTimer from "@/components/timer";
+import Cancellation from "../../components/ui/Cancellation";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function matchPending() {
+  const router = useRouter();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleCancellation = () => {
+    router.push("/");
+    // TODO: Add API Call to count cancellation for user
+    setIsModalOpen(false);
+  };
+
   return (
     <>
       <Header logoUrl="/assets/images/header-logo.png" />
       <main className="p-5 w-11/12 items-center mx-auto flex flex-col justify-center mb-20">
         <div className="-ml-8 flex flex-row items-start w-full px-5 my-10 space-x-5">
-          <BackButton />
+          <BackButton onClick={() => setIsModalOpen(true)} />
           {/* TODO: Call cancellation button again here */}
           <h1 className="text-black font-bold flex flex-col justify-start text-4xl -mt-2">
             Matching in progress!
@@ -32,6 +45,22 @@ export default function matchPending() {
             Your payment will be held during this time.
           </p>
         </div>
+        <Cancellation
+          title="We are still finding you a match, are you sure you want to leave?"
+          imageSrc="/assets/images/gasping_pau.png"
+          imageAltDesc="Paupau looking like he is waiting for an answer"
+          description="Heads up! If you leave this matching session, your cart will be cleared, the restaurant might not remain available as an option."
+          optionOne={{
+            label: "Yes, I want to leave.",
+            onClick: handleCancellation,
+          }}
+          optionTwo={{
+            label: "No, keep me in the pool.",
+            onClick: () => setIsModalOpen(false),
+          }}
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+        />
       </main>
     </>
   );
