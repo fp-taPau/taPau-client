6ff807fdbc3294e813b00ad58e2661960e8857e2
Rachel Gina <rgabelarde@gmail.com>
2024-12-17 15:15:34 +0800

    Update matching in progress page, pending cancellation modal update to be called

--- app/match_pending/page.js
+++ app/match_pending/page.js
@@ -1,19 +1,35 @@
 "use client";
 
+import BackButton from "@/components/ui/BackButton";
 import Header from "../../components/Header";
 import CountdownTimer from "@/components/timer";
 
 export default function matchPending() {
-    return (
-        <>
-            <Header logoUrl="/assets/images/header-logo.png" />
-            <main className="p-5 w-11/12 items-center mx-auto my-20 flex flex-col justify-center">
-                <h1 className="text-partyPink font-bold p-5 w-11/12 items-center mx-auto my-20 flex flex-col justify-center text-4xl">
-                    matching in progress!
-                <img src="/assets/images/curious_pau.png" className="w-100 h-100 items-center"/>
-                </h1>
-                <CountdownTimer/>
-            </main>
-        </>
-    )
+  return (
+    <>
+      <Header logoUrl="/assets/images/header-logo.png" />
+      <main className="p-5 w-11/12 items-center mx-auto flex flex-col justify-center mb-20">
+        <div className="-ml-8 flex flex-row items-start w-full px-5 my-10 space-x-5">
+          <BackButton />
+          {/* TODO: Call cancellation button again here */}
+          <h1 className="text-black font-bold flex flex-col justify-start text-4xl -mt-2">
+            Matching in progress!
+          </h1>
+        </div>
+        <div className="py-8 bg-secondaryPink w-11/12 flex flex-col items-center justify-center rounded-lg shadow-md">
+          <img
+            src="/assets/images/curious_pau.png"
+            className="w-100 h-100 items-center"
+          />
+          <h2 className="text-4xl font-extrabold text-partyPink">
+            Hang Tight!
+          </h2>
+          <p className="text-secondaryText text-center">
+            We are currently looking for a match for you!
+          </p>
+          <CountdownTimer />
+        </div>
+      </main>
+    </>
+  );
 }
